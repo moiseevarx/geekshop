df858da5e9d5282fb8306b752b40a76559845848
moiseevarx <moiseevarx@gmail.com>
2019-06-16 12:12:01 +0300

    change .gitignore

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def main(request):
-    return render(request, 'mainapp/main.html', {'username' : 'roxana'})
+    return render(request, 'mainapp/main.html', {'username': 'roxana'})
+
 
 def products(request):
     return render(request, 'mainapp/products.html')
+
 
 def contacts(request):
     return render(request, 'mainapp/contacts.html')
