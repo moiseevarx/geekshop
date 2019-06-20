49040ebe6299baf64310f34e1d32e042f317619d
moiseevarx <moiseevarx@gmail.com>
2019-06-20 22:46:59 +0300

    Добавлены Product и ProductCategory. Models migration created.

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+import json, os
 
-# Create your views here.
+JSON_PATH = 'mainapp/json'
+
+def loadMenuFromJSON():
+    with open(os.path.join(JSON_PATH, 'menu.json'), 'r') as infile:
+        return json.load(infile)
+
 def main(request):
-    return render(request, 'mainapp/main.html', {'username' : 'roxana'})
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu, 'username': 'roksana', 'array': [1,2,3,4,5]}
+    return render(request, 'mainapp/main.html', {'username': 'roxana'})
 
 def products(request):
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu}
     return render(request, 'mainapp/products.html')
 
 def contacts(request):
+    links_menu = loadMenuFromJSON()
+    context = {'links_menu': links_menu}
     return render(request, 'mainapp/contacts.html')
