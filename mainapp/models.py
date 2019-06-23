2da96d13f42efa74713fbe0740aecc64c3d9e95b
moiseevarx <moiseevarx@gmail.com>
2019-06-23 10:41:21 +0300

    восстановлено меню перехода. Рабочая версия

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class ProductCategory(models.Model):
     class Meta:
         verbose_name = 'Category'
@@ -11,6 +12,7 @@ class ProductCategory(models.Model):
     def __str__(self):
         return self.name
 
+
 class Product(models.Model):
     class Meta:
         verbose_name = 'Product'
@@ -19,11 +21,10 @@ class Product(models.Model):
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
     name = models.CharField(verbose_name="product name", max_length=55)
     image = models.ImageField(upload_to='products_images', blank=True)
-    short_desc = models.CharField(verbose_name="", max_length=60,blank=True)
-    description = models.TextField(verbose_name="product description", blank = True)
+    short_desc = models.CharField(verbose_name="", max_length=60, blank=True)
+    description = models.TextField(verbose_name="product description", blank=True)
     price = models.DecimalField(verbose_name="price", max_digits=8, decimal_places=2, default=0)
-    quantity = models.PositiveIntegerField(verbose_name="storage", default = 0)
+    quantity = models.PositiveIntegerField(verbose_name="storage", default=0)
 
     def __str__(self):
         return "{} ({})".format(self.name, self.category.name)
-
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
-import json, os
+import json
+import os
+from .models import Product
+from .models import ProductCategory
 
 JSON_PATH = 'mainapp/json'
+
 
 def loadMenuFromJSON():
     with open(os.path.join(JSON_PATH, 'menu.json'), 'r') as infile:
         return json.load(infile)
 
+
 def main(request):
     links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu, 'username': 'roksana', 'array': [1,2,3,4,5]}
-    return render(request, 'mainapp/main.html', {'username': 'roxana'})
+    context = {'links_menu': links_menu, 'username': 'roksana', 'array': [1, 2, 3, 4, 5]}
+    print(links_menu)
+    return render(request, 'mainapp/main.html', context)
+
 
 def products(request):
     links_menu = loadMenuFromJSON()
-    context = {'links_menu': links_menu}
-    return render(request, 'mainapp/products.html')
+    context = {'links_menu': links_menu, 'products': Product.objects.all()}
+    print(links_menu)
+    return render(request, 'mainapp/products.html', context)
+
 
 def contacts(request):
     links_menu = loadMenuFromJSON()
     context = {'links_menu': links_menu}
-    return render(request, 'mainapp/contacts.html')
+    return render(request, 'mainapp/contacts.html', context)
